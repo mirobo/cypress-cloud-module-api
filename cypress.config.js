bf8d80dbcaed7a28bd83ff7a809626e299f7ba09
Miro Alexander Rutscho <miro.rutscho@postfinance.ch>
2024-07-03 13:47:43 +0200

    remove anything cypress cloud related

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,7 @@
 const { defineConfig } = require('cypress');
-const { cloudPlugin } = require('cypress-cloud/plugin');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      return cloudPlugin(on, config);
     },
   },
 });
--- getCiBuildId.js
+++
@@ -1,9 +0,0 @@
-function getCiBuildId() {
-  if (process.env.GITLAB_CI) {
-    return `${process.env.CI_COMMIT_REF_NAME}-${process.env.CI_PIPELINE_ID}`;
-  } else {
-    return new Date().toISOString();
-  }
-}
-console.log(getCiBuildId());
-module.exports = getCiBuildId;
